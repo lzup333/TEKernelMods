dabba5fdd0d4eaefea17790383b8ffec6fb70edf
lzup333 <3463541577@qq.com>
2026-09-03 17:46:08 +0800

    添加协议头

--- RandomLoot/main.c
+++ RandomLoot/main.c
@@ -1,4 +1,21 @@
 //
+// Copyright (C) 2026 lzup333
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+//
 // OneHitLoot - 一击必爆
 // NewEFMod (tefkernel / KernelLoader) 重写版，适配 Terraria 1.4.5.x (手机端 / PE)
 //
